175c60f14d055a7e7301085be4972c0ce1de39cb
jonathan.cardenas <jonathan.cardenas@solucionatica.pe>
2023-06-26 09:34:21 -0500

    agrego try catch

--- index.js
+++ index.js
@@ -9,9 +9,18 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/get", async (req, res) => {
-  const snapshot = await User.get();
-  const list = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-  res.send(list);
+
+  try {
+
+    const snapshot = await User.get();
+    const list = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+    return res.send(list);
+    
+  } catch (error) {
+    console.log(error);
+    return res.send(error);
+  }
+  
 });
 
 app.get("/health", async (req, res) => {
@@ -19,23 +28,49 @@ app.get("/health", async (req, res) => {
 });
 
 app.post("/create", async (req, res) => {
-  const data = req.body;
-  await User.add({ data });
-  res.send({ msg: "User Added" });
+
+  try {
+
+    const data = req.body;
+    await User.add({ data });
+    return res.send({ msg: "User Added" });
+      
+  } catch (error) {
+    console.log(error);
+    return res.send(error); 
+  }
+  
 });
 
 app.post("/update", async (req, res) => {
-  const id = req.body.id;
-  delete req.body.id;
-  const data = req.body;
-  await User.doc(id).update(data);
-  res.send({ msg: "Updated" });
+
+  try {
+
+    const id = req.body.id;
+    delete req.body.id;
+    const data = req.body;
+    await User.doc(id).update(data);
+    return res.send({ msg: "Updated" });
+    
+  } catch (error) {
+    console.log(error);
+    return res.send(error);
+  }
+
+  
 });
 
 app.post("/delete", async (req, res) => {
-  const id = req.body.id;
-  await User.doc(id).delete();
-  res.send({ msg: "Deleted" });
+
+  try {
+    const id = req.body.id;
+    await User.doc(id).delete();
+    return res.send({ msg: "Deleted" });
+  } catch (error) {
+    console.log(error);
+    return res.send(error);
+  }
+
 });
 
 app.listen(port, () => console.log("Up & RUnning " + port));
